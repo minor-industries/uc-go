79459934fa773792c73f02019bca1beb552c8027
suurkivi <136639517+suurkivi@users.noreply.github.com>
2023-05-16 10:37:08 -0700

    change pins

--- exe/ir/main.go
+++ exe/ir/main.go
@@ -5,8 +5,11 @@ import (
 	"tinygo.org/x/drivers/irremote"
 )
 
-const pinIRIn = machine.GP17
-const powerPin = machine.GP18
+//const pinIRIn = machine.GP17
+//const powerPin = machine.GP18
+
+const pinIRIn = machine.GP21 // TODO: should be config?
+const powerPin = machine.GP22
 
 func Main(ch irremote.CommandHandler) {
 	powerPin.Configure(machine.PinConfig{Mode: machine.PinOutput})
