f562e62ef260bd220adfd4da5d41587b32abd5bb
suurkivi <136639517+suurkivi@users.noreply.github.com>
2023-11-16 17:28:00 -0800

    working with qtpy

---
+++ app/bbq/cfg_qtpy.go
@@ -0,0 +1,57 @@
+//go:build qtpy
+
+package bbq
+
+import (
+	"machine"
+	rfm69_board "uc-go/pkg/rfm69-board"
+	"uc-go/pkg/spi"
+)
+
+var cfg = BoardCfg{
+	Rfm: rfm69_board.PinCfg{
+		Spi: &spi.Config{
+			Spi: &machine.SPI0,
+			Config: &machine.SPIConfig{
+				Mode: 0,
+				SCK:  machine.SPI0_SCK_PIN,
+				SDO:  machine.SPI0_SDO_PIN,
+				SDI:  machine.SPI0_SDI_PIN,
+			},
+			Cs: machine.A1,
+		},
+		Rst:  machine.NoPin,
+		Intr: machine.NoPin,
+	},
+
+	led: machine.NoPin,
+
+	Tcs: []*ThermocoupleCfg{
+		{
+			Name: "bbq01-meat",
+			Spi: &spi.Config{
+				Spi: &machine.SPI0,
+				Config: &machine.SPIConfig{
+					Mode: 1,
+					SCK:  machine.SPI0_SCK_PIN,
+					SDO:  machine.SPI0_SDO_PIN,
+					SDI:  machine.SPI0_SDI_PIN,
+				},
+				Cs: machine.A0,
+			},
+		},
+		//{
+		//	Name: "bbq01-bbq",
+		//	Spi: &spi.Config{
+		//		Spi: &machine.SPI0,
+		//		Config: &machine.SPIConfig{
+		//			Mode: 1,
+		//			SCK:  machine.A3,
+		//			SDO:  machine.A4,
+		//			SDI:  machine.A1,
+		//		},
+		//		Cs: machine.A0, // TODO:
+		//	},
+		//},
+	},
+}
