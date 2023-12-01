d00a07247ee4c243a218d3a4bf3765ef3a4aff2d
suurkivi <136639517+suurkivi@users.noreply.github.com>
2023-11-30 19:00:31 -0800

    I think EIC waking from deep sleep

--- app/lowpower/main.go
+++ app/lowpower/main.go
@@ -22,7 +22,7 @@ func main() {
 	go bl.Run()
 	bl.Seq([]int{2, 2})
 
-	<-time.After(10 * time.Second)
+	<-time.After(5 * time.Second)
 	bl.Seq([]int{2, 4})
 
 	intr := machine.D1
@@ -45,22 +45,51 @@ func main() {
 
 func configGCLK6() {
 	sam.GCLK.CLKCTRL.ClearBits(sam.GCLK_CLKCTRL_CLKEN)
+	waitClockSync()
+
+	//sam.GCLK.CLKCTRL.Set(sam.GCLK_CLKCTRL_ID_EIC<<sam.GCLK_CLKCTRL_ID_Pos |
+	//	sam.GCLK_CLKCTRL_GEN_GCLK0<<sam.GCLK_CLKCTRL_GEN_Pos |
+	//	sam.GCLK_CLKCTRL_CLKEN)
+	//waitClockSync()
+
+	// *****************
+	sam.GCLK.GENDIV.Set(6 << sam.GCLK_GENDIV_ID_Pos)
+	waitClockSync()
+
+	sam.GCLK.GENCTRL.Set((6 << sam.GCLK_GENCTRL_ID_Pos) |
+		(sam.GCLK_GENCTRL_SRC_OSCULP32K << sam.GCLK_GENCTRL_SRC_Pos) |
+		sam.GCLK_GENCTRL_RUNSTDBY |
+		sam.GCLK_GENCTRL_GENEN)
+	waitClockSync()
+
+	sam.GCLK.CLKCTRL.Set((sam.GCLK_CLKCTRL_ID_EIC << sam.GCLK_CLKCTRL_ID_Pos) |
+		(sam.GCLK_CLKCTRL_GEN_GCLK6 << sam.GCLK_CLKCTRL_GEN_Pos) |
+		sam.GCLK_CLKCTRL_CLKEN)
+	waitClockSync()
+	// *****************
+
+	//sam.GCLK.CLKCTRL.Set(sam.GCLK_CLKCTRL_ID_EIC<<sam.GCLK_CLKCTRL_ID_Pos |
+	//	sam.GCLK_CLKCTRL_GEN_GCLK0<<sam.GCLK_CLKCTRL_GEN_Pos |
+	//	sam.GCLK_CLKCTRL_CLKEN)
+	//waitClockSync()
+
+	//sam.GCLK.CLKCTRL.SetBits(sam.GCLK_CLKCTRL_CLKEN | sam.GCLK_CLKCTRL_GEN_GCLK6 | sam.GCLK_CLKCTRL_ID_EIC)
+	//waitClockSync()
+	//}
+
+	//sam.GCLK.GENCTRL.Set(sam.GCLK_GENCTRL_RUNSTDBY | sam.GCLK_GENCTRL_)
+	//waitClockSync()
+	//}
+
+	sam.GCLK.CLKCTRL.SetBits(sam.GCLK_CLKCTRL_CLKEN)
+	waitClockSync()
+	//
+	//sam.NVMCTRL.CTRLB.SetBits(sam.NVMCTRL_CTRLB_SLEEPPRM_DISABLED)
+}
+
+func waitClockSync() {
 	for sam.GCLK.STATUS.HasBits(sam.GCLK_STATUS_SYNCBUSY) {
 	}
-
-	sam.GCLK.CLKCTRL.SetBits(sam.GCLK_CLKCTRL_CLKEN | sam.GCLK_CLKCTRL_GEN_GCLK6 | sam.GCLK_CLKCTRL_ID_EIC)
-	for sam.GCLK.STATUS.HasBits(sam.GCLK_STATUS_SYNCBUSY) {
-	}
-
-	sam.GCLK.GENCTRL.Set(sam.GCLK_GENCTRL_GENEN | sam.GCLK_GENCTRL_SRC_OSCULP32K | 6)
-	for sam.GCLK.STATUS.HasBits(sam.GCLK_STATUS_SYNCBUSY) {
-	}
-
-	sam.GCLK.GENCTRL.Set(sam.GCLK_GENCTRL_RUNSTDBY)
-	for sam.GCLK.STATUS.HasBits(sam.GCLK_STATUS_SYNCBUSY) {
-	}
-
-	sam.NVMCTRL.CTRLB.SetBits(sam.NVMCTRL_CTRLB_SLEEPPRM_DISABLED)
 }
 
 /*
@@ -87,6 +116,6 @@ static void configGCLK6()
 */
 
 func sleep() {
-	//arm.SCB.SCR.SetBits(arm.SCB_SCR_SLEEPDEEP)
+	arm.SCB.SCR.SetBits(arm.SCB_SCR_SLEEPDEEP)
 	arm.Asm("wfi")
 }
