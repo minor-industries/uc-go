68d0f60c82695cdde392756f1329be6d3f687354
suurkivi <136639517+suurkivi@users.noreply.github.com>
2023-11-30 18:04:17 -0800

    WIP: low power testing

--- app/lowpower/main.go
+++ app/lowpower/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"device/arm"
+	"device/sam"
 	"fmt"
 	"machine"
 	"time"
@@ -31,10 +32,59 @@ func main() {
 		bl.Seq([]int{2, 2, 2, 16})
 	}
 
+	configGCLK6()
+	//
+	//sam.PM.SetSLEEP_IDLE()
+	sam.EIC.WAKEUP.Set(0x0000FFFF)
+	sam.PM.APBAMASK.SetBits(sam.PM_APBAMASK_GCLK_ | sam.PM_APBAMASK_EIC_)
+
 	for {
 		sleep()
 	}
 }
+
+func configGCLK6() {
+	sam.GCLK.CLKCTRL.ClearBits(sam.GCLK_CLKCTRL_CLKEN)
+	for sam.GCLK.STATUS.HasBits(sam.GCLK_STATUS_SYNCBUSY) {
+	}
+
+	sam.GCLK.CLKCTRL.SetBits(sam.GCLK_CLKCTRL_CLKEN | sam.GCLK_CLKCTRL_GEN_GCLK6 | sam.GCLK_CLKCTRL_ID_EIC)
+	for sam.GCLK.STATUS.HasBits(sam.GCLK_STATUS_SYNCBUSY) {
+	}
+
+	sam.GCLK.GENCTRL.Set(sam.GCLK_GENCTRL_GENEN | sam.GCLK_GENCTRL_SRC_OSCULP32K | 6)
+	for sam.GCLK.STATUS.HasBits(sam.GCLK_STATUS_SYNCBUSY) {
+	}
+
+	sam.GCLK.GENCTRL.Set(sam.GCLK_GENCTRL_RUNSTDBY)
+	for sam.GCLK.STATUS.HasBits(sam.GCLK_STATUS_SYNCBUSY) {
+	}
+
+	sam.NVMCTRL.CTRLB.SetBits(sam.NVMCTRL_CTRLB_SLEEPPRM_DISABLED)
+}
+
+/*
+static void configGCLK6()
+{
+	// enable EIC clock
+	GCLK->CLKCTRL.bit.CLKEN = 0; //disable GCLK module
+	while (GCLK->STATUS.bit.SYNCBUSY);
+
+	GCLK->CLKCTRL.reg = (uint16_t) (GCLK_CLKCTRL_CLKEN | GCLK_CLKCTRL_GEN_GCLK6 | GCLK_CLKCTRL_ID( GCM_EIC )) ;  //EIC clock switched on GCLK6
+	while (GCLK->STATUS.bit.SYNCBUSY);
+
+	GCLK->GENCTRL.reg = (GCLK_GENCTRL_GENEN | GCLK_GENCTRL_SRC_OSCULP32K | GCLK_GENCTRL_ID(6));  //source for GCLK6 is OSCULP32K
+	while (GCLK->STATUS.reg & GCLK_STATUS_SYNCBUSY);
+
+	GCLK->GENCTRL.bit.RUNSTDBY = 1;  //GCLK6 run standby
+	while (GCLK->STATUS.reg & GCLK_STATUS_SYNCBUSY);
+
+	Errata: Make sure that the Flash does not power all the way down
+     	* when in sleep mode.
+
+	NVMCTRL->CTRLB.bit.SLEEPPRM = NVMCTRL_CTRLB_SLEEPPRM_DISABLED_Val;
+}
+*/
 
 func sleep() {
 	//arm.SCB.SCR.SetBits(arm.SCB_SCR_SLEEPDEEP)
