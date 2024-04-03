4bc5ec09bd2ceb74aa215dca10d8176fce48e870
suurkivi <136639517+suurkivi@users.noreply.github.com>
2024-04-03 16:11:00 -0700

    use external neopixel-spi

--- pkg/leds/leds_samd.go
+++ pkg/leds/leds_samd.go
@@ -4,8 +4,8 @@ package leds
 
 import (
 	"device/sam"
-	"github.com/minor-industries/uc-go/pkg/neopixel-spi/driver"
-	"github.com/minor-industries/uc-go/pkg/neopixel-spi/driver/default_driver"
+	neopixel_spi "github.com/minor-industries/neopixel-spi"
+	"github.com/minor-industries/neopixel-spi/default_driver"
 	"github.com/pkg/errors"
 	"image/color"
 	"machine"
@@ -13,8 +13,8 @@ import (
 
 var TxFullCounter int64 // TODO
 
-func Setup(numLeds int) (*driver.NeoSpiDriver, error) {
-	d := default_driver.Configure(&driver.Cfg{
+func Setup(numLeds int) (*neopixel_spi.NeoSpiDriver, error) {
+	d := default_driver.Configure(&neopixel_spi.Cfg{
 		SPI:        &machine.SPI{Bus: sam.SERCOM5_SPIM, SERCOM: 5},
 		SCK:        machine.PA22, // 5.1 (sercom alt)
 		SDO:        machine.PA23, // 5.0 (sercom alt)
@@ -31,6 +31,6 @@ func Setup(numLeds int) (*driver.NeoSpiDriver, error) {
 }
 
 func Write(driver_ any, pixels []color.RGBA) {
-	d := driver_.(*driver.NeoSpiDriver)
+	d := driver_.(*neopixel_spi.NeoSpiDriver)
 	d.Animate(pixels)
 }
